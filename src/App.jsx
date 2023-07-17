af702d7ac9f2ee0f42ec07485a7c788e81ba5c1e
Brien-Austin <brienaustinclayton@gmail.com>
2023-07-17 23:20:08 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import NavBar from './components/NavBar'
+
+const App = () => {
+  return (
+    <>
+      <NavBar/>
+    <div className='bg-indigo-600 w-full h-screen'>
+    
+      </div>
+      </>
+  )
+}
+
+export default App
